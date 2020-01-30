0249e842e1a89d5592a0ea9278c7305a06679337
MateoLa <mateo.laino@gmail.com>
2020-01-30 13:20:12 -0300

    first

--- app/assets/javascripts/spree_avatar/use_avatar_ikon.js
+++ app/assets/javascripts/spree_avatar/use_avatar_ikon.js
@@ -1,27 +1,26 @@
 Spree.ready(function($) {
-  var deletePictureButtons = document.querySelectorAll('.js-delete-avatar-button');
-  if (deletePictureButtons.length > 0) {
-    deletePictureButtons.forEach(function(deleteButton) {
-      deleteButton.addEventListener('click', function(e) {
-        document.querySelector('#overlay').classList.add('shown');
-        document.querySelector('#delete-avatar-popup').classList.add('shown');
-        document.querySelector('#delete-avatar-popup-confirm').href = e.currentTarget.dataset.avatar;
-      }, false)
-    })
+  var useAvatarIkonBoxes = document.querySelectorAll('.js-use-avatar-as-ikon');
+  if (useAvatarIkonBoxes.length > 0) {
+    var useAvatarIconBox = useAvatarIkonBoxes.first;
   }
 
-  document.querySelector('#overlay').addEventListener('click', function () {
-    var addressActionElement = document.querySelector('#delete-avatar-popup');
-    if (addressActionElement) addressActionElement.classList.remove('shown');
-  }, false);
-
-  var popupCloseButtons = document.querySelectorAll('.js-delete-avatar-popup-close-button')
-  if (popupCloseButtons.length > 0) {
-    popupCloseButtons.forEach(function(closeButton) {
-      closeButton.addEventListener('click', function(e) {
-        document.querySelector('#overlay').classList.remove('shown');
-        document.querySelector('#delete-avatar-popup').classList.remove('shown');
-      })
-    })
+  useAvatarIconBox.click(function(e){
+    var user_id = e.currentTarget.dataset.user_id;
+    SwitchIkon(user_id);
   }
 })
+
+
+function SwitchIkon(user){
+  $.ajax({
+    url: "/account/" + user + "avatar_ikon",
+    method: "post",
+    success: function (data, status, xhr) {
+      // success callback function
+      flashDiv = $('<div class="alert alert-' + data.type + '" />');
+      $('#content').prepend(flashDiv);
+      flashDiv.html(data.msg).show().delay(5000).slideUp()
+    }
+  });
+  return false
+}
