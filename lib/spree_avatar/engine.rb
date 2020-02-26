52520558ea4fcff2d590a87e96f9b3d25f02c723
MateoLa <mateo.laino@gmail.com>
2020-02-26 19:38:07 -0300

    first

--- lib/spree_avatar/engine.rb
+++ lib/spree_avatar/engine.rb
@@ -2,11 +2,6 @@ module SpreeAvatar
   class Engine < Rails::Engine
     engine_name 'spree_avatar'
 
-  	initializer "spree_avatar.assets.precompile" do |app|
-#      app.config.assets.paths << File.expand_path("../../assets/images", __FILE__)
-	    app.config.assets.precompile += %w( spree_avatar/*.svg )
-    end
-
     config.to_prepare do
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
